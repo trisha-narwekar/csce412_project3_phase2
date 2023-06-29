f3e0b8cf8146ab73e89dffbeacb3af97addd4620
trisha-narwekar <trishaprtn@gmail.com>
2023-06-29 00:57:48 -0500

    change 2

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
    <div className="bg-white h-screen w-screens grid grid-cols-2">
     <div><p className="mt-96 text-5xl font-bold text-center align-middle text-gray-900">hello world</p></div>
-    <a href="http://csce412tnarwekar.xyz/personal-website/index.html"><div class="mx-24 mt-96 bg-gray-900 h-8 w-72 font-bold text-center text-xl color-black rounded-xl">click here for website!</div></a>
+    <a href="http://csce412tnarwekar.xyz/personal-website/index.html"><div class="mx-24 mt-96 bg-red-900 h-8 w-72 font-bold text-center text-xl color-black rounded-xl">click here for website!</div></a>
    </div>
 
   )
