78c23da3f6f780cda6dba958fb7d522074fd2d25
trisha-narwekar <trishaprtn@gmail.com>
2023-06-29 00:12:57 -0500

    changing to black button

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
    <div className="bg-white h-screen w-screens grid grid-cols-2">
     <div><p className="mt-96 text-5xl font-bold text-center align-middle text-gray-900">hello world</p></div>
-    <a href="http://csce412tnarwekar.xyz/personal-website/index.html"><div class="mx-24 mt-96 bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 h-8 w-72 font-bold text-center text-xl color-black rounded-lg">click here for website!</div></a>
+    <a href="http://csce412tnarwekar.xyz/personal-website/index.html"><div class="mx-24 mt-96 bg-gray-900 h-8 w-72 font-bold text-center text-xl color-black rounded-lg">click here for website!</div></a>
    </div>
 
   )
